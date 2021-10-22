2adc1ef306e376c2a1ca28565c345489d8afca53
Vineet Bharot <vbharot@circinus-5.ics.uci.edu>
2021-10-21 17:27:22 -0700

    feat: add test casese for building nd ;

--- discussions/3/test.sh
+++ discussions/3/test.sh
@@ -1,13 +1,26 @@
 ls /home
+rm -rf x.txt
 echo "238P is cool" > x.txt
 cat < x.txt
-rm redir.out
+rm -rf redir.out
 echo "hello from test.sh" | wc -c > redir.out
+cat redir.out
 ls | sort | uniq | wc -c
 ls /home | sort | uniq | wc -l
-rm usernames.txt
+rm -rf usernames.txt
 ls /home > usernames.txt
-rm users_with_v.txt
+rm -rf users_with_v.txt
 cat < usernames.txt | sort | uniq | grep v > users_with_v.txt
 echo usernames with v:
 cat users_with_v.txt
+cd /home/vbharot/cs238p/discussions/3/new_folder
+echo "current location"
+pwd
+cd ..
+echo "current location"
+pwd
+history | grep txt
+history > his.txt
+cat < his.txt
+echo 1 ; echo 2 ; echo 3 ; echo all done
+ls > lis.txt ; history > his.txt ; wc < lis.txt ; wc < his.txt ; history | grep echo ; echo done
